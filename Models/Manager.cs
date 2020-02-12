8800682326cea26048b9d4ddf988cb44c2c19639
unknown <you@example.com>
2020-02-12 12:12:53 +0400

    First Commit,Create Proect,folders and Login Form

---
+++ Models/Manager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackEnd_Final_Project.Models
+{
+    public class Manager
+    {
+        public int id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+    }
+}
